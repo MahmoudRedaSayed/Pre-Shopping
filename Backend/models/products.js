a66b51db361664b76171116798333eb326965592
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-08-08 12:30:13 +0200

    working on cart screen and edit countInstock in the database

--- Backend/models/products.js
+++ Backend/models/products.js
@@ -41,7 +41,7 @@ const productSchema=mongoose.Schema({
         required:true,
         default:0
     }
-    ,numInStocks:{
+    ,countInStock:{
         type:Number,
         required:true,
         default:0
